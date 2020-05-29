4afa05b891bc2942a071cab53f43a3bcf279ecd6
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-05-29 23:27:45 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -14,11 +14,11 @@ async def amireallyalive(alive):
     await alive.edit("**MY BOT IS RUNNING SUCCESFULLY**\n\n"
                      "`☞Telethon version: 1.11.3\n`"
                      "`☞Python: 3.8.2\n`"
-                     "`☞Bot was modified by:` sandeep\n"
+                     "`☞Bot was modified by:` @Sur_vivor\n"
                      "`☞and created by :` snapdragon,anubis\n"
                      "`☞Database Status: Databases functioning normally!\n\n`"
                      "`☞Always with you, my master!\n`"
-                     f"`☞My peru owner`: [{DEFAULTUSER}](https://github.com/sandy1709/catuserbot)\n"
+                     f"`☞My peru owner`: [{DEFAULTUSER}](https://github.com/Sur-vivor/CatUserbot)\n"
                      #"[Deploy this userbot Now](https://github.com/sandy1709/catuserbot)"
                     )
 
