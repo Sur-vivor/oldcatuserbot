f9454a2c2facacc24f72bc921ccfb2c6e09b2298
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-02 15:17:45 +0530

    Update autoprofile.py

--- userbot/plugins/autoprofile.py
+++ userbot/plugins/autoprofile.py
@@ -46,8 +46,8 @@ async def autopic(event):
         current_time = datetime.now().strftime("⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡ \n  Time: %H:%M \n  Date: %d.%m.%y \n⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡")
         img = Image.open(photo)
         drawn_text = ImageDraw.Draw(img)
-        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 35)
-        drawn_text.text((250, 250), current_time, font=fnt, fill=(255, 255, 0))
+        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
+        drawn_text.text((400, 400), current_time, font=fnt, fill=(255, 255, 0))
         img.save(photo)
         file = await bot.upload_file(photo)  # pylint:disable=E0602
         try:
