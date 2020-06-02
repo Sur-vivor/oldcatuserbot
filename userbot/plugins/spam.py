728ad18ec11c2f7e7afe5b2a7d5ff5d0e136b88c
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-02 21:56:37 +0530

    Update spam.py

--- userbot/plugins/spam.py
+++ userbot/plugins/spam.py
@@ -23,7 +23,22 @@ async def tmeme(e):
         await e.client.send_message(
             BOTLOG_CHATID, "#CSPAM\n"
             "TSpam was executed successfully")
-
+        
+@register(outgoing=True, pattern="^.bigspam")
+async def bigspam(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        message = e.text
+        counter = int(message[9:13])
+        spam_message = str(e.text[13:])
+        for i in range(1, counter):
+            await e.respond(spam_message)
+        await e.delete()
+        if BOTLOG:
+            await e.client.send_message(
+                BOTLOG_CHATID,
+                "#BIGSPAM \n\n"
+                "Bigspam was executed successfully"
+                )
 
 @register(outgoing=True, pattern="^.wspam (.*)")
 async def tmeme(e):
@@ -85,6 +100,8 @@ CMD_HELP.update({
     ".cspam <text>\
 \nUsage: Spam the text letter by letter.\
 \n\n.spam <count> <text>\
+\nUsage: Floods text in the chat !!\
+\n\n.bigspam <count> <text>\
 \nUsage: Floods text in the chat !!\
 \n\n.wspam <text>\
 \nUsage: Spam the text word by word.\
