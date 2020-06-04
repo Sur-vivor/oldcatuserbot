5d6904dd7f19c31f49ec288c4f7754073779ffe6
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-04 14:35:14 +0530

    Add files via upload

---
+++ userbot/plugins/elautopfp.py
@@ -0,0 +1,65 @@
+"""
+Time In Profile Pic.....
+Command: `.epp`
+
+:::::Credit Time::::::
+1) Coded By: @s_n_a_p_s
+2) Ported By: @r4v4n4 (Legend)
+3) End Game Help By: @spechide
+NOTE: NO.4 IS A VIRUS WHICH HAD COME HERE ACCIDENTALLY
+4) Custom / Modified Plugin for some magical effects by this Legendary Guy @PhycoNinja13b 
+
+
+#curse: who ever edits this credit section will goto hell
+
+⚠️DISCLAIMER⚠️
+
+USING THIS PLUGIN CAN RESULT IN ACCOUNT BAN + CAS BAN + SPAM BAN + ACCOUNT SUSPENSION . WE DONT CARE ABOUT BAN, SO WE ARR USING THIS.
+"""
+import os
+from datetime import datetime
+from PIL import Image, ImageDraw, ImageFont
+from pySmartDL import SmartDL
+from telethon.tl import functions
+from uniborg.util import admin_cmd
+import asyncio
+import shutil 
+import random, re
+
+
+FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
+
+#Add telegraph media links of profile pics that are to be used
+TELEGRAPH_MEDIA_LINKS = ["https://telegra.ph/file/b20273be27aeea4e8fcac.jpg",
+                        ]
+@borg.on(admin_cmd(pattern="epp ?(.*)"))
+async def autopic(event):
+    while True:
+        piclink = random.randint(0, len(TELEGRAPH_MEDIA_LINKS) - 1)
+        AUTOPP = TELEGRAPH_MEDIA_LINKS[piclink]
+        downloaded_file_name = "./ravana/original_pic.png"
+        downloader = SmartDL(AUTOPP, downloaded_file_name, progress_bar=True)
+        downloader.start(blocking=False)
+        photo = "photo_pfp.png"
+        while not downloader.isFinished():
+            place_holder = None
+    
+    
+        shutil.copy(downloaded_file_name, photo)
+        im = Image.open(photo)
+        current_time = datetime.now().strftime("@Sur_vivor \n \nTime: %H:%M:%S \nDate: %d/%m/%y")
+        img = Image.open(photo)
+        drawn_text = ImageDraw.Draw(img)
+        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 23)
+        drawn_text.text((50, 450), current_time, font=fnt, fill=(230,230,250))
+        img.save(photo)
+        file = await event.client.upload_file(photo)  # pylint:disable=E0602
+        try:
+            await event.client(functions.photos.UploadProfilePhotoRequest(  # pylint:disable=E0602
+                file
+            ))
+            os.remove(photo)
+            
+            await asyncio.sleep(30)
+        except:
+            return
