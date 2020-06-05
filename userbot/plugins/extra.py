442688810be0cbf9438714db7f69d0e9949ca977
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-06 00:46:57 +0530

    Update extra.py

--- userbot/plugins/extra.py
+++ userbot/plugins/extra.py
@@ -14,7 +14,7 @@ from userbot.utils import admin_cmd
 @borg.on(admin_cmd(pattern="leave"))
 async def leave(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("`I iz Leaving dis Kensur Group kek!`")
+        await e.edit("`Legend is leaving this chat.....!` @admin `Goodbye aren't forever..` ")
         time.sleep(3)
         if '-' in str(e.chat_id):
             await bot(LeaveChannelRequest(e.chat_id))
