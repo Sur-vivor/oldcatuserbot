b2f21e72cea605638e15e90d9c59de59a28d9985
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-11 14:18:42 +0530

    Update autoprofile.py

--- userbot/plugins/autoprofile.py
+++ userbot/plugins/autoprofile.py
@@ -66,7 +66,7 @@ async def _(event):
     while True:
         DM = time.strftime("%d-%m-%y")
         HM = time.strftime("%H:%M")
-        name = f"⌚{HM} ⚡️{DEFAULTUSER}⚡️ 📆{DM}"
+        name = f"🕒{HM} ⚡️{DEFAULTUSER}⚡️ 📆{DM}"
         logger.info(name)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
