2a22a975ebe14b1263e87b663743542cff39e16e
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-14 21:52:57 +0530

    Update fun.py

--- userbot/plugins/fun.py
+++ userbot/plugins/fun.py
@@ -13,7 +13,7 @@ from telethon.tl.types import MessageEntityMentionName
 
 
 from userbot import CMD_HELP
-from userbot.utils import register
+from userbot.utils import register, admin_cmd
 
 
 # ================= CONSTANT =================
@@ -38,7 +38,7 @@ GAMBAR_TITIT = """
 
 # ===========================================
 
-@register(outgoing=True, pattern="^.(yes|no|maybe|decide)$")
+@borg.on(admin_cmd(outgoing=True, pattern="^.(yes|no|maybe|decide)$"))
 async def decide(event):
     decision = event.pattern_match.group(1).lower()
     message_id = event.reply_to_msg_id if event.reply_to_msg_id else None
@@ -54,17 +54,17 @@ async def decide(event):
 
 
 
-@register(outgoing=True, pattern="^.fp$")
+@borg.on(admin_cmd(outgoing=True, pattern="^.fp$"))
 async def facepalm(e):
     """ Facepalm  🤦‍♂ """
     await e.edit("🤦‍♂")
 
-@register(outgoing=True, pattern="^.corona$")
+@borg.on(admin_cmd(outgoing=True, pattern="^.corona$"))
 async def iqless(e):
     await e.edit("Antivirus scan was completed \n⚠️ Warning! This  donkey has Corona Virus")
 
 
-@register(outgoing=True, pattern="^.ggl (.*)")
+@borg.on(admin_cmd(outgoing=True, pattern="^.ggl (.*)"))
 async def let_me_google_that_for_you(lmgtfy_q):
     textx = await lmgtfy_q.get_reply_message()
     qry = lmgtfy_q.pattern_match.group(1)
@@ -81,7 +81,7 @@ async def let_me_google_that_for_you(lmgtfy_q):
     \n[{query}]({r.json()['shorturl']})")
 
 
-@register(pattern=r".scam(?: |$)(.*)", outgoing=True)
+@borg.on(admin_cmd(pattern=r".scam(?: |$)(.*)", outgoing=True))
 async def scam(event):
     """ Just a small command to fake chat actions for fun !! """
     options = [
