d1f5d77253d72e65c873737fb204cd9eba7ba3dd
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-17 13:48:06 +0530

    Update fedstat.py

--- userbot/plugins/fedstat.py
+++ userbot/plugins/fedstat.py
@@ -24,12 +24,12 @@ async def _(event):
     if event.reply_to_msg_id:
         chat = await event.get_reply_message()
     sysarg = event.pattern_match.group(1)
-    if sysarg == "":
+    if sysarg == chat.user.id:
       async with borg.conversation(bot) as conv:
           try:
               await conv.send_message("/start")
               response = await conv.get_response()
-              await conv.send_message("/fedstat")
+              await conv.send_message("/fedstat" + sysarg)
               audio = await conv.get_response()
               final = ("If you would like to know more about the fedban reason in a specific federation, use /fbanstat <FedID> in RoseBot." , "")
               await borg.send_message(event.chat_id, audio.text)
@@ -60,16 +60,4 @@ async def _(event):
               await event.delete()
           except YouBlockedUserError:
               await event.edit("**Error:** `unblock` @MissRose_Bot `and try again!")
-    elif sysarg == chat.user.id:
-      async with borg.conversation(bot) as conv:
-          try:
-              await conv.send_message("/start")
-              response = await conv.get_response()
-              await conv.send_message("/fedstat")
-              audio = await conv.get_response()
-              final = ("If you would like to know more about the fedban reason in a specific federation, use /fbanstat <FedID> in RoseBot." , "")
-              await borg.send_message(event.chat_id, audio.text)
-              await event.delete()
-          except YouBlockedUserError:
-              await event.edit("**Error:** `unblock` @MissRose_bot `and retry!")            
-                
+   
