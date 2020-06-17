db2c355dc4bdd1a994d61de1dc7e4de1de0f645e
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-17 13:42:19 +0530

    Update fedstat.py

--- userbot/plugins/fedstat.py
+++ userbot/plugins/fedstat.py
@@ -21,6 +21,8 @@ bot = "@MissRose_bot"
 async def _(event):
     if event.fwd_from:
         return
+    if event.reply_to_msg_id:
+        chat = await event.get_reply_message()
     sysarg = event.pattern_match.group(1)
     if sysarg == "":
       async with borg.conversation(bot) as conv:
@@ -57,4 +59,17 @@ async def _(event):
               await borg.send_message(event.chat_id, audio.text)
               await event.delete()
           except YouBlockedUserError:
-              await event.edit("**Error:** `unblock` @MissRose_Bot `and try again!")                         
+              await event.edit("**Error:** `unblock` @MissRose_Bot `and try again!")
+    elif sysarg == chat.user.id:
+      async with borg.conversation(bot) as conv:
+          try:
+              await conv.send_message("/start")
+              response = await conv.get_response()
+              await conv.send_message("/fedstat")
+              audio = await conv.get_response()
+              final = ("If you would like to know more about the fedban reason in a specific federation, use /fbanstat <FedID> in RoseBot." , "")
+              await borg.send_message(event.chat_id, audio.text)
+              await event.delete()
+          except YouBlockedUserError:
+              await event.edit("**Error:** `unblock` @MissRose_bot `and retry!")            
+                
